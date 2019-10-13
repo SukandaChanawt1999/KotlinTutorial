b93cb0a12ca4e5195863b56c2c41ac3172f1c1c2
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-14 01:51:17 +0700

    4.3.Variables and Types

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>){
     println("Hello World")
     println("My name is Sukanda Chanawat")
 
-    var cs: String
+//    var cs: String
     cs = "Computer Science"
     println(cs)
 }

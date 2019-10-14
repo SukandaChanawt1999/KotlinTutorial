ccc597ce77a5fa40f58e0a2557412b9b028e6667
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-14 11:17:08 +0700

    8.Conditions

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -1,31 +1,5 @@
 fun main(args: Array<String>){
-    println("Hello World")
-    println("My name is Sukanda Chanawat")
-    val sc = "Sukanda Chanawat"
-    val cs = "Computer Science"
-    println(cs)
 
-    val csWeeklySalary = 32
-    val csMonthlySalary = csWeeklySalary * 4
-    println("Cs's $ weekly salary is $$csWeeklySalary")
-    println("Monthly, that comes to $csMonthlySalary")
-
-    println()
-
-    val apples = 6
-    val oranges = 5
-    val fruit = apples - oranges
-    println("$apples apples - $oranges oranges leaves $fruit piece(s) of fruit")
-
-    println("A quarter of the apples is ${apples / 4 } " )
-
-    println()
-    val weeks = 234
-    val years = weeks / 52.0
-    println("$weeks weeks is $years years")
-
-    println("My name is $cs")
-    println("sc is $sc")
 
 
 }

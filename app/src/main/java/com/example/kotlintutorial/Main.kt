60b235c46250eb466f882599785952168058105e
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-14 11:36:53 +0700

    8.8 Conditions

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -12,4 +12,14 @@ fun main(args: Array<String>){
     val age = readLine()!!.toInt()
     println("age is $age")
 
+    val message: String
+    if (age < 18){
+        message = "You're too young to vote"
+    }else if (age == 100){
+        message = "Congratulations"
+    }else{
+        message = "You can vote"
+    }
+    println(message)
+
 }

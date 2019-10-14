abc1769ff7293749ad36ae783c69bbb2b190eb32
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-14 11:31:40 +0700

    8.7 Conditions

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -1,12 +1,15 @@
 fun main(args: Array<String>){
-    val lives = 0
-    var isGameOver = (lives < 1)
-    println(isGameOver)
-    if (isGameOver ){
-        println("Game over!")
-    }else{
-        println("You're still alive!")
-    }
+//    val lives = 0
+//    var isGameOver = (lives < 1)
+//    println(isGameOver)
+//    if (isGameOver ){
+//        println("Game over!")
+//    }else{
+//        println("You're still alive!")
+//    }
 
+    println("How old are you: ")
+    val age = readLine()!!.toInt()
+    println("age is $age")
 
 }

f15b6f3d126808e69fc00f4c970123e577452a05
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-18 02:35:46 +0700

    12.2 Lists

--- app/src/main/java/com/example/kotlintutorial/Player.kt
+++ app/src/main/java/com/example/kotlintutorial/Player.kt
@@ -1,9 +1,11 @@
 package com.example.kotlintutorial
 
+import Loot
 import Weapon
 
 class Player(val name: String,var level: Int = 1, var lives: Int = 3, var score: Int = 0 ) {
     var weapon: Weapon = Weapon("Fist",1)
+    val inventory = ArrayList<Loot>()
 
     fun show(){
         println("""

efda084b0941e0d7218de45b3555e97c4465d9c8
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-18 03:07:36 +0700

    15.4 for Loops

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -1,52 +1,55 @@
 import com.example.kotlintutorial.Player
 
 fun main(args: Array<String>){
-    val tim = Player("Tim")
-//    println(tim.name)
-//    println(tim.lives)
-//    println(tim.level)
-//    println(tim.score)
-    tim.show()
-
-    val louise = Player("Louise",5)
-    louise.level = 5
-    louise.show()
-
-    val gr8 = Player("gr8",4,8)
-    val one2watch = Player("Ace",2,5,1000)
-    gr8.show()
-    one2watch.show()
-
-    println(one2watch.weapon.name.toUpperCase())
-    println(one2watch.weapon.damageInflicted)
-
-    val axe= Weapon("Axe",12)
-    gr8.weapon = axe
-    println(gr8.weapon.name)
-    println(axe.name)
-
-    axe.damageInflicted = 24
-    println(axe.damageInflicted)
-    println(gr8.weapon.damageInflicted)
-
-    tim.weapon = Weapon("Sword",10)
-//    println(tim.weapon.name)
-    tim.show()
-
-    louise.weapon = tim.weapon
-    louise.show()
-
-    tim.weapon = Weapon("Spear",14)
-//    println(tim.weapon.name)
-    tim.show()
-
-    val redPotion = Loot("Red Potion",LootType.POTION,7.50)
-    tim.inventory.add(redPotion)
-    val chesArmor = Loot("+3 Chest Armor",LootType.ARMOR,80.00)
-    tim.inventory.add(chesArmor)
-    tim.showInventory()
-    tim.inventory.add(Loot("Ring of Protection +2",LootType.RING,40.25))
-    tim.inventory.add(Loot("Invisibility Potion",LootType.POTION,35.95))
-    tim.showInventory()
-    println(tim.toString())
+    for (i in 1..10){
+        println(i)
+    }
+//    val tim = Player("Tim")
+////    println(tim.name)
+////    println(tim.lives)
+////    println(tim.level)
+////    println(tim.score)
+//    tim.show()
+//
+//    val louise = Player("Louise",5)
+//    louise.level = 5
+//    louise.show()
+//
+//    val gr8 = Player("gr8",4,8)
+//    val one2watch = Player("Ace",2,5,1000)
+//    gr8.show()
+//    one2watch.show()
+//
+//    println(one2watch.weapon.name.toUpperCase())
+//    println(one2watch.weapon.damageInflicted)
+//
+//    val axe= Weapon("Axe",12)
+//    gr8.weapon = axe
+//    println(gr8.weapon.name)
+//    println(axe.name)
+//
+//    axe.damageInflicted = 24
+//    println(axe.damageInflicted)
+//    println(gr8.weapon.damageInflicted)
+//
+//    tim.weapon = Weapon("Sword",10)
+////    println(tim.weapon.name)
+//    tim.show()
+//
+//    louise.weapon = tim.weapon
+//    louise.show()
+//
+//    tim.weapon = Weapon("Spear",14)
+////    println(tim.weapon.name)
+//    tim.show()
+//
+//    val redPotion = Loot("Red Potion",LootType.POTION,7.50)
+//    tim.inventory.add(redPotion)
+//    val chesArmor = Loot("+3 Chest Armor",LootType.ARMOR,80.00)
+//    tim.inventory.add(chesArmor)
+//    tim.showInventory()
+//    tim.inventory.add(Loot("Ring of Protection +2",LootType.RING,40.25))
+//    tim.inventory.add(Loot("Invisibility Potion",LootType.POTION,35.95))
+//    tim.showInventory()
+//    println(tim.toString())
 }

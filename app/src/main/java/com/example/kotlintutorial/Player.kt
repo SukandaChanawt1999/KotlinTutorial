e0ec18a5aa9edf7682d5bb67531589955f64e5df
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-18 01:24:08 +0700

    9.3 Classes

--- app/src/main/java/com/example/kotlintutorial/Player.kt
+++ app/src/main/java/com/example/kotlintutorial/Player.kt
@@ -4,4 +4,13 @@ class Player(val name: String) {
     var lives = 3
     var level = 1
     var score = 0
+
+    fun show(){
+        println("""
+            name: $name
+            lives: $lives
+            level: $level
+            score: $score
+            """)
+    }
 }

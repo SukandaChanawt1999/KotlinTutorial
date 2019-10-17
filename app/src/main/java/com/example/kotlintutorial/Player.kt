49d07dd613a05088c7b6a14afeb7001d58adc6ec
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-18 02:47:37 +0700

    13.3 More on Lists

--- app/src/main/java/com/example/kotlintutorial/Player.kt
+++ app/src/main/java/com/example/kotlintutorial/Player.kt
@@ -8,14 +8,21 @@ class Player(val name: String,var level: Int = 1, var lives: Int = 3, var score:
     val inventory = ArrayList<Loot>()
 
     fun show(){
-        println("""
+        if (lives > 0){
+            println("$name is alive")
+        }else{
+            println("$name is dead")
+        }
+    }
+    override fun toString(): String {
+        return """
             name: $name
             lives: $lives
             level: $level
             score: $score
             weapon: ${weapon.name}
             damage: ${weapon.damageInflicted}
-            """)
+            """
     }
 
     fun showInventory(){

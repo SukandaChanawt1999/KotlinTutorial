83c8e44a20f51f914c1cb35bc116e89d37e1142f
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-18 01:25:53 +0700

    9.4 Classes

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -2,9 +2,10 @@ import com.example.kotlintutorial.Player
 
 fun main(args: Array<String>){
     val tim = Player("Tim")
-    println(tim.name)
-    println(tim.lives)
-    println(tim.level)
-    println(tim.score)
+//    println(tim.name)
+//    println(tim.lives)
+//    println(tim.level)
+//    println(tim.score)
+    tim.show()
 
 }

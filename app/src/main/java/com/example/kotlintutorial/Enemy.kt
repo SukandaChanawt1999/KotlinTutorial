1224def4c2318a6e8fbe022fc113d928cbad71de
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-18 09:48:47 +0700

    18.3 Inheritance

--- app/src/main/java/com/example/kotlintutorial/Enemy.kt
+++ app/src/main/java/com/example/kotlintutorial/Enemy.kt
@@ -1,5 +1,14 @@
 package com.example.kotlintutorial
 
-class Enemy (val name: String, var hitPoints: Int, var lives: Int){
-
+open class Enemy (val name: String, var hitPoints: Int, var lives: Int){
+    fun takeDamage(damage: Int){
+        val remainingHitPoints = hitPoints - damage
+        if (remainingHitPoints > 0 ){
+            hitPoints = remainingHitPoints
+            println("$name took $damage points of damage, and has $hitPoints left")
+        }else{
+            lives -=1
+            println("$name lost a lite")
+        }
+    }
 }

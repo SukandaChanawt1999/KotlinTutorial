3af97ed438e5e606add295f68c706690c2068568
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-18 10:58:27 +0700

    23.1 for Loop Revisited

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -1,6 +1,7 @@
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.example.kotlintutorial.Player
 import com.example.kotlintutorial.Troll
 import com.example.kotlintutorial.Vampyre
 import com.example.kotlintutorial.VampyreKing
@@ -9,28 +10,41 @@ import com.example.kotlintutorial.VampyreKing
 @RequiresApi(Build.VERSION_CODES.N)
 fun main(args: Array<String>){
 
-    val uglyTroll = Troll("Ugly Troll")
-    println(uglyTroll)
-    uglyTroll.takeDamage(30)
-    println(uglyTroll)
+    val conan = Player("Conan")
+    conan.getLoot(Loot("Invisibility", LootType.POTION,4.0))
+    conan.getLoot(Loot("Mithril", LootType.ARMOR,183.0))
+    conan.getLoot(Loot("Ring of speed", LootType.RING,25.0))
+    conan.getLoot(Loot("Red Potion", LootType.POTION,2.0))
+    conan.getLoot(Loot("Cursed Shield", LootType.ARMOR,-8.0))
+    conan.getLoot(Loot("Brass Ring", LootType.RING,1.0))
+    conan.getLoot(Loot("Chin Mail", LootType.ARMOR,4.0))
+    conan.getLoot(Loot("Gold Ring", LootType.RING,12.0))
+    conan.getLoot(Loot("Health Potion", LootType.POTION,3.0))
+    conan.getLoot(Loot("Silver Ring", LootType.RING,6.0))
+    conan.showInventory()
 
-    val vlad = Vampyre("Vlad")
-    println(vlad)
-    vlad.takeDamage(8)
-    println(vlad)
+//    val uglyTroll = Troll("Ugly Troll")
+//    println(uglyTroll)
+//    uglyTroll.takeDamage(30)
+//    println(uglyTroll)
+//
+//    val vlad = Vampyre("Vlad")
+//    println(vlad)
+//    vlad.takeDamage(8)
+//    println(vlad)
+//
+//    val dracula = VampyreKing("Dracula")
+//    println(dracula)
 
-    val dracula = VampyreKing("Dracula")
-    println(dracula)
-
-    while (dracula.lives > 0){
-        if (dracula.dodges()){
-            continue;
-        }
-        if (dracula.runAway()){
-            println("Dracula ran away")
-            break
-        }else{
-            dracula.takeDamage(12)
-        }
-    }
+//    while (dracula.lives > 0){
+//        if (dracula.dodges()){
+//            continue;
+//        }
+//        if (dracula.runAway()){
+//            println("Dracula ran away")
+//            break
+//        }else{
+//            dracula.takeDamage(12)
+//        }
+//    }
 }
